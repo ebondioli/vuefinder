543e90cba3ed32c8bc234d9700cb10abfae58de6
Emilio Bondioli <emilio@leva.io>
2024-03-01 17:33:59 +0100

    add preview of video files

--- src/components/Explorer.vue
+++ src/components/Explorer.vue
@@ -103,6 +103,9 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
               </svg>
               <img class="lazy h-10 md:h-12 m-auto" v-else-if="(item.mime_type ?? '').startsWith('image')" :data-src="app.requester.getPreviewUrl(app.adapter, item)" :alt="item.basename">
+              <video muted loop class="h-10 md:h-12 m-auto" v-else-if="(item.mime_type ?? '').startsWith('video')" @mouseenter="playVideoPreview" @mouseleave="pauseVideoPreview" :alt="item.basename">
+                <source :src="app.requester.getPreviewUrl(app.adapter, item)" type="video/mp4" />
+              </video>
               <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 md:h-12 md:w-12 m-auto text-neutral-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
               </svg>
@@ -341,6 +344,17 @@ const setDragSelect = () => {
     selectedCount.value = ds.value.getSelection().length;
   })
 };
+
+function playVideoPreview(event) {
+  console.log(event)
+  event.target.play()
+}
+
+function pauseVideoPreview(event) {
+  console.log(event)
+  event.target.pause()
+  event.target.currentTime = 0
+}
 
 onMounted(() => {
   vfLazyLoad = new LazyLoad(selectorArea.value);
